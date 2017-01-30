503e8c4aaf5d9731611775f701112b2cf5d7f81c
Paul Colomiets <paul@colomiets.name>
2017-01-30 15:36:14 +0200

    Add `spawn`, `spawn_fn` and `run_forever` helpers

--- src/lib.rs
+++ src/lib.rs
@@ -60,7 +60,7 @@ extern crate tokio_core;
 
 use std::time::{Duration, Instant};
 
-use futures::IntoFuture;
+use futures::{IntoFuture, Future, empty};
 use tokio_core::reactor::{Core, Handle, Timeout, Interval};
 
 
@@ -96,6 +96,11 @@ pub fn is_running() -> bool {
 /// lp.run(futures::lazy(f))
 /// ```
 ///
+/// The difference between `run()` and `run_forever()` is merely a convenience.
+/// Basically, you want to use `run()` in client applications when you have
+/// a future that should complete to proceed. And `run_forever()` in server
+/// applications which spawns some listeners and never exits.
+///
 /// But also initializes thread-local loop handle for the time of loop run
 pub fn run<F: FnOnce() -> R, R: IntoFuture>(f: F)
     -> Result<R::Item, R::Error>
@@ -103,6 +108,30 @@ pub fn run<F: FnOnce() -> R, R: IntoFuture>(f: F)
     let mut lp = Core::new().expect("create loop");
     HANDLE.set(&lp.handle(), || {
         lp.run(futures::lazy(f))
+    })
+}
+
+/// Run the main loop and initialize it by running a function, which spawns
+/// more futures in the main loop. Then run loop indefinitely.
+///
+/// This is basically a shortcut for:
+///
+/// ```ignore
+/// let mut lp = Core::new().expect("create loop");
+/// lp.run(futures::lazy(f)
+///        .and_then(|_| empty()))
+/// ```
+///
+/// The difference between `run()` and `run_forever()` is merely a convenience.
+/// Basically, you want to use `run()` in client applications when you have
+/// a future that should complete to proceed. And `run_forever()` in server
+/// applications which spawns some listeners and never exits.
+///
+/// But also initializes thread-local loop handle for the time of loop run
+pub fn run_forever<F: FnOnce() -> Result<(), E>, E>(f: F) -> Result<(), E> {
+    let mut lp = Core::new().expect("create loop");
+    HANDLE.set(&lp.handle(), || {
+        lp.run(futures::lazy(f).and_then(|_| empty()))
     })
 }
 
@@ -164,4 +193,45 @@ pub fn interval(dur: Duration) -> Interval {
     HANDLE.with(|handle| {
         Interval::new(dur, handle).unwrap()
     })
+}
+
+/// Spawn a future to the current main loop
+///
+/// This only works if running inside the `run()` function of the main loop
+///
+/// This is an equivalent of:
+///
+/// ```ignore
+/// handle().spawn(f)
+/// ```
+///
+/// # Panics
+///
+/// This function panics if there is no currently running loop (i.e. this
+/// function is not running from the inside of `run()`.
+pub fn spawn<F>(f: F)
+    where F: Future<Item=(), Error=()> + 'static
+{
+    HANDLE.with(|handle| handle.spawn(f))
+}
+
+/// Spawn a closure to the current main loop
+///
+/// This only works if running inside the `run()` function of the main loop
+///
+/// This is an equivalent of:
+///
+/// ```ignore
+/// handle().spawn_fn(f)
+/// ```
+///
+/// # Panics
+///
+/// This function panics if there is no currently running loop (i.e. this
+/// function is not running from the inside of `run()`.
+pub fn spawn_fn<F, R>(f: F)
+    where F: FnOnce() -> R + 'static,
+          R: IntoFuture<Item=(), Error=()> + 'static
+{
+    HANDLE.with(|handle| handle.spawn_fn(f))
 }
